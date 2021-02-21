149af565a0b78c96a69cfa2af57d115975883d37
kabuk1 <leslie.liszcz@gmail.com>
2021-02-21 06:28:07 +0000

    Registration form added

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:helping_hand/services/auth.dart';
 
-class SignIn extends StatefulWidget {
+class Register extends StatefulWidget {
   final Function toggleView;
-  SignIn({this.toggleView});
-
+  Register({this.toggleView});
   @override
-  _SignInState createState() => _SignInState();
+  _RegisterState createState() => _RegisterState();
 }
 
-class _SignInState extends State<SignIn> {
+class _RegisterState extends State<Register> {
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
 
   //text field state
   String email = '';
   String password = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
@@ -23,28 +24,34 @@ class _SignInState extends State<SignIn> {
         appBar: AppBar(
             backgroundColor: Colors.purple[400],
             elevation: 0.0,
-            title: Text('Sign in to Helping Hand'),
+            title: Text('Sign up to Helping Hand'),
             actions: <Widget>[
               FlatButton.icon(
                   icon: Icon(Icons.person),
-                  label: Text('Register'),
+                  label: Text('Sign In'),
                   onPressed: () {
                     widget.toggleView();
                   })
             ]),
         body: Container(
-          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.9),
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
           child: Form(
+            key: _formKey,
             child: Column(
               children: <Widget>[
                 SizedBox(height: 20.0),
-                TextFormField(onChanged: (val) {
-                  setState(() => email = val);
-                }),
+                TextFormField(
+                    validator: (val) => val.isEmpty ? 'Enter an email' : null,
+                    onChanged: (val) {
+                      setState(() => email = val);
+                    }),
                 // textedFormField
                 SizedBox(height: 20.0),
                 TextFormField(
                     obscureText: true,
+                    validator: (val) => val.length < 6
+                        ? 'Enter a password with 6 or more characters.'
+                        : null,
                     onChanged: (val) {
                       setState(() => password = val);
                     }),
@@ -54,13 +61,24 @@ class _SignInState extends State<SignIn> {
                 RaisedButton(
                     color: Colors.pink[400],
                     child: Text(
-                      'Sign in',
+                      'Register',
                       style: TextStyle(color: Colors.white),
                     ),
                     onPressed: () async {
-                      print(email);
-                      print(password);
-                    })
+                      if (_formKey.currentState.validate()) {
+                        dynamic result = await _auth
+                            .registerWithEmailAndPassword(email, password);
+                        if (result == null) {
+                          setState(
+                              () => error = 'Please supply a valid email.');
+                        }
+                      }
+                    }),
+                SizedBox(height: 12.0),
+                Text(
+                  error,
+                  style: TextStyle(color: Colors.red, fontSize: 16.0),
+                )
               ],
             ),
           ),
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,13 +1,40 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:helping_hand/models/user.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  
+
+  //create user object on Firebase
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
+
+  // auth change user stream
+  Stream<User> get user {
+    return _auth.onAuthStateChanged.map(_userFromFirebaseUser);
+  }
   // sign in with email and password
 
+  // register with email and password
+  Future registerWithEmailAndPassword(String email, String password) async {
+    try {
+      AuthResult result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
 
-// register with email and password
-
-// sign out
-
+  // sign out
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
 }
